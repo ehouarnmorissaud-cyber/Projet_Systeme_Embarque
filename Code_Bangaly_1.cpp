8954f89c580c49f23fba6b886f700c08115777f7
benga2004 <bangalykaba0810@gmail.com>
2025-11-02 14:49:15 +0100

    Update Code_Bangaly_1.cpp

--- Code_Bangaly_1.cpp
+++ Code_Bangaly_1.cpp
@@ -9,7 +9,8 @@
 
 // Définition des broches
 #define PIN_LED 4            // LED multicolore (pilotée par PWM ou multiplexage)
-#define PIN_BOUTON 2         // Bouton d’interruption
+#define PIN_BOUTON_VERT 2  
+#define PIN_BOUTON_ROUGE 3 // Button vert
 #define PIN_LUMIERE A0       // Capteur de lumière
 #define PIN_PRESSION A1      // Capteur de pression analogique
 #define PIN_SD_CS 10         // Chip Select de la carte SD
